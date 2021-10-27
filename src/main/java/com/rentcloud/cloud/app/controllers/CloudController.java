4284517e57aaef3a8b1f09e650df973dc13372f1
LDvargashidalgo <laura.vargas.mt@correo.usa.edu.co>
2021-10-27 12:04:17 -0500

    Actualizar edad en cloud service

--- src/main/java/com/rentcloud/cloud/app/controllers/CloudController.java
+++ src/main/java/com/rentcloud/cloud/app/controllers/CloudController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.rentcloud.cloud.app.controllers;
 import com.rentcloud.cloud.app.entities.Cloud;
 import com.rentcloud.cloud.app.services.CloudService;
